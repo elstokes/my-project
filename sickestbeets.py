d507165c28c70d52c4b02e4b7203701cad61b468
elstokes <ethan_stokes@davincischools.org>
2017-02-23 14:50:52 -0800

    Add files via upload

--- sickestbeets.py
+++ sickestbeets.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO #We add the GPIO library to our script so we can use the pins on our RPi.
 import time #Remember from our GoPiGo times, we need to use time.sleep() to control the flow of our code (so it isn't all executed at once!)
+import pygame
 
 GPIO.setmode(GPIO.BCM) #This means that you are referring to the pins by the numbers after "GPIO".
 #The first answer here goes into more detail --> http://raspberrypi.stackexchange.com/questions/12966/what-is-the-difference-between-board-and-bcm-for-gpio-pin-numbering
@@ -19,8 +20,7 @@ print "Press:\n\tq:Send secret code\n\tw:8th note\n\te:quarter note\n\tr:half no
 while True:
     print "Enter the Command:",
     
-       
-   
+    
     a=raw_input()   # Fetch the input from the terminal
     
     if a=='w':
@@ -38,33 +38,13 @@ while True:
             GPIO.output(4,GPIO.LOW)
        	    GPIO.output(3,GPIO.LOW)
        	    GPIO.output(2,GPIO.LOW)		
-    
-    elif a=='e':
-            print "Half beat"
-   	    GPIO.output(18,GPIO.HIGH)
-       	    time.sleep(Whole/2)
-       	    GPIO.output(18,GPIO.LOW)
-       	    time.sleep(0.1)
-   	    print "Dun"
-    
-    elif a=='r':
-            print "Quarter beat"
-   	    GPIO.output(18,GPIO.HIGH)
-       	    time.sleep(Whole/4)
-       	    GPIO.output(18,GPIO.LOW)
-       	    time.sleep(0.1)
-   	    print "Dun"
-    
-    elif a=='t':
-            print "Eighth beat"
-   	    GPIO.output(18,GPIO.HIGH)
-       	    time.sleep(Whole/8)
-       	    GPIO.output(18,GPIO.LOW)
-       	    time.sleep(0.1)
-   	    print "Dun"
    	    	
     elif a=='q':
             print "SEIZURE WARNING"
+            pygame.mixer.init()
+            pygame.mixer.music.load("Just a Bit Crazy.mp3")
+            pygame.mixer.music.play()
+            
             print "1"
             GPIO.output(18,GPIO.HIGH)
        	    time.sleep(.48)
@@ -74,9 +54,9 @@ while True:
        	    time.sleep(.48)
        	    GPIO.output(18,GPIO.LOW)
        	    time.sleep(.48)
-       	    
+           	    
        	    print "1"
-            GPIO.output(18,GPIO.HIGH)
+       	    GPIO.output(18,GPIO.HIGH)
        	    time.sleep(.48)
        	    GPIO.output(18,GPIO.LOW)
        	    time.sleep(.48)
@@ -84,9 +64,9 @@ while True:
        	    time.sleep(.48)
        	    GPIO.output(18,GPIO.LOW)
        	    time.sleep(.48)
-       	    
+           	    
        	    print "1"
-            GPIO.output(18,GPIO.HIGH)
+       	    GPIO.output(18,GPIO.HIGH)
        	    time.sleep(.48)
        	    GPIO.output(18,GPIO.LOW)
        	    time.sleep(.48)
@@ -106,7 +86,7 @@ while True:
        	    time.sleep(.12)
        	    GPIO.output(18,GPIO.HIGH)
        	    time.sleep(.48)
-       	    
+           	    
        	    print "1"
             GPIO.output(18,GPIO.HIGH)
        	    time.sleep(.48)
@@ -116,7 +96,7 @@ while True:
        	    time.sleep(.48)
        	    GPIO.output(4,GPIO.LOW)
        	    time.sleep(.48)
-       	    
+           	    
        	    print "1"
        	    GPIO.output(3,GPIO.HIGH)
        	    time.sleep(.48)
@@ -126,7 +106,7 @@ while True:
        	    time.sleep(.48)
        	    GPIO.output(2,GPIO.LOW)
        	    time.sleep(.48)
-       	    
+           	    
        	    print "1"
             GPIO.output(18,GPIO.HIGH)
        	    time.sleep(.48)
@@ -146,7 +126,7 @@ while True:
        	    time.sleep(.48)
        	    GPIO.output(3,GPIO.LOW)
        	    time.sleep(.48)
-       	    
+           	    
        	    print "1"
             GPIO.output(4,GPIO.HIGH)
        	    time.sleep(.48)
@@ -158,7 +138,7 @@ while True:
        	    time.sleep(.48)
        	    
        	    print "1"
-            GPIO.output(4,GPIO.HIGH)
+       	    GPIO.output(4,GPIO.HIGH)
        	    time.sleep(.48)
        	    GPIO.output(4,GPIO.LOW)
        	    time.sleep(.48)
@@ -176,7 +156,7 @@ while True:
        	    time.sleep(.48)
        	    GPIO.output(18,GPIO.LOW)
        	    time.sleep(.48)
-       	    
+           	    
        	    print "1"
             GPIO.output(4,GPIO.HIGH)
        	    time.sleep(.48)
@@ -186,12 +166,12 @@ while True:
        	    time.sleep(.48)
        	    GPIO.output(3,GPIO.LOW)
        	    time.sleep(.48)
-       	    
+           	    
        	    GPIO.output(18,GPIO.HIGH)
        	    GPIO.output(4,GPIO.HIGH)
        	    GPIO.output(3,GPIO.HIGH)
        	    GPIO.output(2,GPIO.HIGH)
-       	    
+           	    
     
     elif a=='z':
         print "Exiting"     # Exit
